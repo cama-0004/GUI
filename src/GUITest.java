f84dcf8592b10e55874a401c926779b1e6a042b8
Jesus Camarena <jesus.camarena-1@ou.edu>
2020-11-18 16:44:10 -0600

    Add GUITest

---
+++ src/GUITest.java
@@ -0,0 +1,56 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GUITest {
+
+	private static final int HEIGHT = 400;
+	private static final int WIDTH = 400;
+
+	public static void main(String[] args) {
+		// so far, all our program were sequential and
+		// they reached the END - final (in spanish lol)
+		JFrame frame = new JFrame("Dark Wave");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		//Clash
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlowLayout layout  = new FlowLayout();
+		frame.setLayout(layout);
+		
+		
+		JButton b1 = new JButton("Man who stare");
+		JButton b2 = new JButton("at goats");
+		JButton b52= new JButton("at code monkeys");
+		JButton doNOTPush = new JButton(
+				"my finger is on the button");
+		b1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(" STOP staring ! ! ");
+			}
+			
+		});
+		
+		
+		frame.add(b1);
+		frame.add(b2);
+		frame.add(b52);
+		frame.add(doNOTPush);
+		
+		frame.pack();
+		
+		frame.setVisible(true);
+		
+		
+		
+		
+		
+	}
+
+}
